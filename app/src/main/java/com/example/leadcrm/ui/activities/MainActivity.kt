5cd17b2cbf3f9221350b6bba2670a5a134fa3617
Najimkhon <qwerty1995.290@gmail.com>
2023-05-03 22:15:41 +0500

    Bottom nav menu is added

--- app/src/main/java/com/example/leadcrm/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/leadcrm/ui/activities/MainActivity.kt
@@ -2,13 +2,25 @@ package com.example.leadcrm.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.leadcrm.R
+import com.example.leadcrm.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        setContentView(view)
     }
 }
