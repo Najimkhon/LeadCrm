d1a8df00561d054e36d628a619ab34f7c0fcc716
Najimkhon <qwerty1995.290@gmail.com>
2023-05-03 17:10:19 +0500

    Navgraph and HomeFragment are created

---
+++ app/src/main/java/com/example/leadcrm/ui/fragments/HomeFragment.kt
@@ -0,0 +1,8 @@
+package com.example.leadcrm.ui.fragments
+
+import com.example.leadcrm.base.BaseFragment
+import com.example.leadcrm.databinding.FragmentHomeBinding
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate){
+
+}
