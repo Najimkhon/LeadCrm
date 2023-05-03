71e1b557880428a1feb36dd59e9bbef1079f81c7
Najimkhon <qwerty1995.290@gmail.com>
2023-05-03 16:59:56 +0500

    Creating ditectories and app class

--- app/src/main/java/com/example/leadcrm/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/leadcrm/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.leadcrm
+package com.example.leadcrm.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.leadcrm.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
