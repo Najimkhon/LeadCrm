3d6658bffe875dc2fbd9c0a8bf3f8ec5dba76fd1
Najimkhon <qwerty1995.290@gmail.com>
2023-05-09 09:24:16 +0500

    Search in country dialog is implemented

--- app/src/main/java/com/example/leadcrm/ui/adapters/CountryAdapter.kt
+++ app/src/main/java/com/example/leadcrm/ui/adapters/CountryAdapter.kt
@@ -3,17 +3,21 @@ package com.example.leadcrm.ui.adapters
 import android.content.Context
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.graphql.CountriesQuery
-import com.example.graphql.LeadsQuery
 import com.example.leadcrm.ui.layouts.CountryItemLayout
-import com.example.leadcrm.ui.layouts.LeadItemLayout
+import java.util.*
 
 class CountryAdapter(
     val context: Context
 ):RecyclerView.Adapter<CountryAdapter.CountryViewHolder>() {
+
+    private val originalList = mutableListOf<CountriesQuery.FetchCountry>()
+
+    private val displayedFilteredList = mutableListOf<CountriesQuery.FetchCountry>()
+
+    private val notYetDisplayedFilteredList = mutableListOf<CountriesQuery.FetchCountry>()
 
     val diffCallback = object : DiffUtil.ItemCallback<CountriesQuery.FetchCountry>() {
         override fun areItemsTheSame(
@@ -21,35 +25,96 @@ class CountryAdapter(
             newItem: CountriesQuery.FetchCountry
         ): Boolean {
             return oldItem.id == newItem.id &&
-                    oldItem.emoji == newItem.emoji &&
-                    oldItem.title == newItem.title &&
-                    oldItem.phoneCode == newItem.phoneCode
+                    oldItem.title == newItem.title
+
         }
 
         override fun areContentsTheSame(
             oldItem: CountriesQuery.FetchCountry,
             newItem: CountriesQuery.FetchCountry
         ): Boolean {
-            return oldItem.hashCode() == newItem.hashCode()
+            return  oldItem == newItem
         }
     }
-
-    val differ = AsyncListDiffer(this, diffCallback)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryViewHolder {
         return CountryViewHolder(CountryItemLayout(context))
     }
 
     override fun getItemCount(): Int {
-        return differ.currentList.size
+        val size = notYetDisplayedFilteredList.size
+        println("hop: getItemCount = $size")
+        return size
     }
 
     override fun onBindViewHolder(holder: CountryViewHolder, position: Int) {
-        val currentCountry = differ.currentList[position]
+        val currentCountry = notYetDisplayedFilteredList[position]
         holder.layout.fillContent(currentCountry)
     }
 
-    fun setData(lessons: List<CountriesQuery.FetchCountry>) = differ.submitList(lessons)
+    fun setData(countries: List<CountriesQuery.FetchCountry>) {
+        originalList.clear()
+        originalList.addAll(countries)
+        filter("")
+    }
+
+    fun filter(query: String) {
+        notYetDisplayedFilteredList.clear()
+        println("hop: query=$query")
+        if (query.trim().isEmpty()) {
+            notYetDisplayedFilteredList.addAll(originalList)
+        } else {
+            val lowerCaseQuery = query.trim().lowercase(Locale.getDefault())
+            for (country in originalList) {
+                if (country.title.lowercase(Locale.getDefault()).contains(lowerCaseQuery)) {
+                    notYetDisplayedFilteredList.add(country)
+                }
+            }
+            println("hop: result size=${notYetDisplayedFilteredList.size}")
+        }
+
+        val diffResult = DiffUtil.calculateDiff(object : DiffUtil.Callback() {
+            override fun getOldListSize(): Int {
+                val oldListSize = displayedFilteredList.size
+                println("hop: oldListSize = $oldListSize")
+                return oldListSize
+            }
+
+            override fun getNewListSize(): Int {
+                val newListSize = notYetDisplayedFilteredList.size
+                println("hop: newListSize = $newListSize")
+                return newListSize
+            }
+
+            override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+                val oldItem = displayedFilteredList[oldItemPosition]
+                val newItem = notYetDisplayedFilteredList[newItemPosition]
+                println("hop: areItemsTheSame:oldItem = $oldItem")
+                println("hop: areItemsTheSame:newItem = $newItem")
+                val areItemsTheSame = oldItem.id == newItem.id
+                println("hop: areItemsTheSame result = $areItemsTheSame")
+                println("hop: =====================")
+                return areItemsTheSame
+            }
+
+            override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+                val oldItem = displayedFilteredList[oldItemPosition]
+                val newItem = notYetDisplayedFilteredList[newItemPosition]
+                println("hop: areContentsTheSame:oldItem = $oldItem")
+                println("hop: areContentsTheSame:newItem = $newItem")
+                val areContentsTheSame = oldItem == newItem
+                println("hop: areContentsTheSame result = $areContentsTheSame")
+                println("hop: =====================")
+                return areContentsTheSame
+            }
+        })
+
+        diffResult.dispatchUpdatesTo(this)
+        displayedFilteredList.clear()
+        displayedFilteredList.addAll(notYetDisplayedFilteredList)
+        println("hop: =======================================================")
+        println("hop: =======================================================")
+    }
 
     inner class CountryViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
         val layout = itemView as CountryItemLayout
--- app/src/main/java/com/example/leadcrm/ui/dialogs/CountriesDialogFragment.kt
+++ app/src/main/java/com/example/leadcrm/ui/dialogs/CountriesDialogFragment.kt
@@ -1,6 +1,9 @@
 package com.example.leadcrm.ui.dialogs
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -25,17 +28,39 @@ class CountriesDialogFragment : BottomSheetDialogFragment() {
     ): View {
         _binding = CountriesBottomSheetDialogBinding.inflate(inflater, container, false)
 
-        viewModel.getCountries()
-        viewModel.countriesLiveData.observe(this){
-            countryAdapter.setData(it)
+        binding.rvCountries.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = countryAdapter
         }
 
-        binding.rvCountries.apply {
-            adapter = countryAdapter
-            layoutManager = LinearLayoutManager(requireContext())
+        viewModel.countriesLiveData.observe(this){
+            Log.d("CountriesDialogFragment", "Received countries: ${it.size}")
+            countryAdapter.setData(it)
         }
-        
+        viewModel.getCountries()
+
+        setupSearchEditText()
+
         return binding.root
+    }
+
+    private fun setupSearchEditText() {
+        binding.etSearchInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Not used
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // Not used
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+                val query = s.toString().trim()
+                Log.d("CountriesDialogFragment", "Search query: $query")
+                countryAdapter.filter(query)
+            }
+        })
     }
 
     override fun onDestroyView() {
