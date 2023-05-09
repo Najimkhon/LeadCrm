5db24f712fc307ddb311b21cba17d47acd4453c5
Najimkhon <qwerty1995.290@gmail.com>
2023-05-09 22:48:37 +0500

    Editable state is added

--- app/src/main/java/com/example/leadcrm/ui/fragments/LeadProfileFragment.kt
+++ app/src/main/java/com/example/leadcrm/ui/fragments/LeadProfileFragment.kt
@@ -1,11 +1,51 @@
 package com.example.leadcrm.ui.fragments
 
+import androidx.core.view.isVisible
 import com.example.leadcrm.base.BaseFragment
 import com.example.leadcrm.databinding.FragmentLeadProfileBinding
 
-class LeadProfileFragment : BaseFragment<FragmentLeadProfileBinding>(FragmentLeadProfileBinding::inflate) {
+class LeadProfileFragment :
+    BaseFragment<FragmentLeadProfileBinding>(FragmentLeadProfileBinding::inflate) {
+
+    override fun setListeners() {
+        binding.generaInfoForm.btnEditGeneralInfo.setOnClickListener{
+            isEditableMode(true)
+        }
+        binding.generaInfoForm.btnSaveGeneralInfo.setOnClickListener{
+            isEditableMode(false)
+        }
+    }
 
     override fun prepareUI() {
-        binding.generaInfoForm.etLeadType.isEnabled = false
+        binding.generaInfoForm.apply {
+            etCountry.isEditable(false)
+            etLanguage.isEditable(false)
+        }
+        isEditableMode(false)
+
     }
+
+    private fun isEditableMode(isEditable: Boolean) {
+        binding.apply {
+            generaInfoForm.apply {
+                etAdSource.isEditable(isEditable)
+                etBirthday.isEditable(isEditable)
+                etBudget.isEditable(isEditable)
+                etChannel.isEditable(isEditable)
+                etCity.isEditable(isEditable)
+                etLeadType.isEditable(isEditable)
+                etWebSource.isEditable(isEditable)
+                etProperty.isEditable(isEditable)
+                etNationality.isEditable(isEditable)
+                etBirthday.isEditable(isEditable)
+                etBudget.isEditable(isEditable)
+                etCityProperty.isEditable(isEditable)
+                etChannelProperty.isEditable(isEditable)
+                etLanguageProperty.isEditable(isEditable)
+                btnEditGeneralInfo.isVisible = !isEditable
+                btnSaveGeneralInfo.isVisible = isEditable
+            }
+        }
+    }
+
 }
